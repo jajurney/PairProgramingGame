c4a21401116839c67331650bb9515cff96b8eb74
jajurney <72029322+jajurney@users.noreply.github.com>
2020-10-27 12:28:11 -0400

    Update Leatherface.cs

--- PairProgramingGame/Enemies/Leatherface.cs
+++ PairProgramingGame/Enemies/Leatherface.cs
@@ -4,30 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static PairProgramingGame.Attack;
-
 namespace PairProgramingGame.Enemies
 {
     public class Leatherface : IEnemy
     {
         public string Name { get; } = "Leatherface";
-
         public int Health { get; set; } = 100;
-
         public int PointValue { get; } = 800;
-
-
         public void Weakness(Attack attack)
         {
-                if (attack.Type == AttackType.Trick || attack.Type == AttackType.Sword)
+            if (attack.Type == AttackType.Trick || attack.Type == AttackType.Sword)
             {
                 Health -= 100;
             }
-            else 
+            if (attack.Type == AttackType.Cross || attack.Type == AttackType.Fire || attack.Type == AttackType.Water || attack.Type == AttackType.Knives)
             {
-                Console.WriteLine("Leatherface is invulnerable to the Attack");
+                Console.WriteLine("Leatherface is invulnerable to the attack");
                 Health -= 0;
             }
-
         }
         public Attack Attack()
         {
