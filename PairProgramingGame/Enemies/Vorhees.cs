5bc93782c272bdf8f7badf7f3fe33e13cc18ba46
jajurney <72029322+jajurney@users.noreply.github.com>
2020-10-27 12:29:36 -0400

    Update Vorhees.cs

--- PairProgramingGame/Enemies/Vorhees.cs
+++ PairProgramingGame/Enemies/Vorhees.cs
@@ -4,32 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static PairProgramingGame.Attack;
-
 namespace PairProgramingGame.Enemies
 {
     public class Vorhees : IEnemy
     {
         public string Name { get; } = "Jason Vorhees";
-
         public int Health { get; set; } = 100;
-
         public int PointValue { get; } = 1000;
-
-
         public void Weakness(Attack attack)
         {
-             if (attack.Type == AttackType.Water)
+            if (attack.Type == AttackType.Water)
             {
                 Health -= 100;
             }
-          if (attack.Type == AttackType.Fire || attack.Type == AttackType.Cross || attack.Type == AttackType.Trick)
+            if (attack.Type == AttackType.Fire || attack.Type == AttackType.Cross || attack.Type == AttackType.Trick)
             {
-                Console.WriteLine("Jason is invulnerable to the attack!");
+                Console.WriteLine("Vorhees is invulnerable to the attack");
                 Health -= 0;
             }
-             if (attack.Type == AttackType.Sword)
+            if (attack.Type == AttackType.Sword)
             {
-                Console.WriteLine("Sword attack did 50% damage to Jason.");
+                Console.WriteLine("Attack inflicted 50% damage to Vorhees");
                 Health -= 50;
             }
         }
@@ -38,7 +33,5 @@ namespace PairProgramingGame.Enemies
             Console.WriteLine("Jason cut you with his Machete");
             return new Attack(10);
         }
-
-
     }
 }
