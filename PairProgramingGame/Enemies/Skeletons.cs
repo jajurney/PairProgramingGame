c54afcc9dfff4521fcac32f5ea1baa590941ab1e
jajurney <72029322+jajurney@users.noreply.github.com>
2020-10-27 12:28:51 -0400

    Update Skeletons.cs

--- PairProgramingGame/Enemies/Skeletons.cs
+++ PairProgramingGame/Enemies/Skeletons.cs
@@ -4,33 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static PairProgramingGame.Attack;
-
 namespace PairProgramingGame.Enemies
 {
     class Skeletons : IEnemy
     {
         public string Name { get; } = "Skeletons";
-
         public int Health { get; set; } = 100;
-
         public int PointValue { get; } = 100;
-
-
         public void Weakness(Attack attack)
         {
-           if (attack.Type == AttackType.Fire || attack.Type == AttackType.Sword || attack.Type == AttackType.Water)
+            if (attack.Type == AttackType.Fire || attack.Type == AttackType.Sword || attack.Type == AttackType.Water)
             {
-                
                 Health -= 100;
             }
-            else if (attack.Type == AttackType.Cross || attack.Type == AttackType.Trick)
+            if (attack.Type == AttackType.Cross || attack.Type == AttackType.Trick)
             {
-                Console.WriteLine("Skeleton is invulnerable to the attack!");
+                Console.WriteLine("Skeleton is invulnerable to the attack");
                 Health -= 0;
             }
-
-
-
         }
         public Attack Attack()
         {
